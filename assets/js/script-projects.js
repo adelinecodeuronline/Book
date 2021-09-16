3701c2bb64a73da30256a1e1b44f7189dc715473
adelinecodeuronline <a.bonnard@codeur.online>
2021-09-16 22:31:55 +0200

    --all

---
+++ assets/js/script-projects.js
@@ -0,0 +1,65 @@
+window.onload = function() {
+
+ 
+////////////drag and scroll section  
+const slider = document.querySelector('.parent');
+let mouseDown = false;
+let startX, scrollLeft;
+
+let startDragging = function (e) {
+  mouseDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+};
+let stopDragging = function (event) {
+  mouseDown = false;
+};
+
+slider.addEventListener('mousemove', (e) => {
+  e.preventDefault();
+  if(!mouseDown) { return; }
+  const x = e.pageX - slider.offsetLeft;
+  const scroll = x - startX;
+  slider.scrollLeft = scrollLeft - scroll;
+});
+
+// Add the event listeners
+slider.addEventListener('mousedown', startDragging, false);
+slider.addEventListener('mouseup', stopDragging, false);
+slider.addEventListener('mouseleave', stopDragging, false);
+
+}
+
+
+
+////////Open overlay's project detail
+//ACS's overlay
+function on() {
+  document.querySelector("#overlay-acs").style.display = "block";
+}
+
+function off() {
+  document.querySelector("#overlay-acs").style.display = "none";
+} 
+
+
+//Azuream's overlay
+function onAzuream() {
+  document.querySelector("#overlay-azuream").style.display = "block";
+}
+
+function offAzuream() {
+  document.querySelector("#overlay-azuream").style.display = "none";
+} 
+
+//Challeng'o' matic's overlay
+function onChallengomatic() {
+  document.querySelector("#overlay-challengomatic").style.display = "block";
+}
+
+function offChallengomatic() {
+  document.querySelector("#overlay-challengomatic").style.display = "none";
+} 
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,78 @@
+window.onload = function() {
 
+//portrait's click
+let img= document.querySelector('#portrait');
+img.addEventListener("click", hoverProfile);
+
+let pictureLink = document.querySelector('#picture');
+pictureLink.addEventListener("click", picture);
+
+let github = document.querySelector('#github');
+github.addEventListener("click", githubLink);
+
+}
+
+
+//portrait's click
+function hoverProfile() {
+    let profile = document.querySelector("#about2");
+    profile.style.display = "block"; 
+}
+
+
+
+//bubble contact
+function bubble() {
+    let popup = document.querySelector("#myPopup");
+    popup.classList.toggle("show");
+  }
+
+  function bubbleOff() {
+    document.querySelector(".popup").style.display = "none";
+  }
+
+
+
+//Profile's overlay
+function onProfile() {
+    document.querySelector("#overlay-profile").style.display = "block";
+  }
+  
+  function offProfile() {
+    document.querySelector("#overlay-profile").style.display = "none";
+  } 
+
+
+
+//CV's overlay
+function cv() {
+    document.querySelector("#overlay-cv").style.display = "block";
+  }
+  
+  function offCv() {
+    document.querySelector("#overlay-cv").style.display = "none";
+  } 
+
+
+//Projects's link
+
+function picture() {
+    window.open('./projets.html', '_blank');
+       
+}
+
+//Github's link
+
+function githubLink() {
+    window.open('https://github.com/adelinecodeuronline', '_blank'); 
+}
+    
+    
+
+
+
+
+
+
+
+  
